8fbef1e4aeafc343efbbe3d4ad0d8b5e4fa51c83
AlexanDER7012 <216101886+AlexanDER7012@users.noreply.github.com>
2026-04-05 00:15:02 -0600

    seguridad

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import * as userController from "../controllers/usuario.controller.js";
-
+import { verificarToken } from "../middlewares/auth.middleware.js";
 const router = Router();
-
+router.use(verificarToken);
 router.get("/", userController.getUsuarios);
 router.get("/:id", userController.getUsuarioById);
 router.post("/create", userController.createUsuario);
